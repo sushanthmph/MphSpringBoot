6c24d774e9a54737b8550f53ec199fbb8c0850da
sushanthmph <sushanth.hp@mphasis.com>
2026-03-31 17:08:09 +0530

    Add files via upload

---
+++ SpringMVCExp3/src/main/java/com/test/controller/BookController.java
@@ -0,0 +1,42 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.test.model.Book;
+
+@Controller
+public class BookController {
+	@GetMapping("/bform")
+	public String bookForm()
+	{
+		return "bookpage";
+	
+	}
+	@PostMapping("/submit")
+	public String submitBookForm(@RequestParam("id")int id,
+			@RequestParam("name")String name,
+			@RequestParam("author")String author,
+			@RequestParam("price")int price,
+			Model model, Book book)
+	{
+		book.setId(id);
+		book.setName(name);
+		book.setPrice(price);
+		book.setAuthor(author);
+		
+		model.addAttribute("data",book);
+		
+		if(book.getId()>0)
+		{
+			return "success";
+		}
+		else {
+			return "bookpage";
+		}
+	}
+
+}
