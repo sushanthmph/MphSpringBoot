d27129f617027e208145b58ef3559e2618a048a4
sushanthmph <sushanth.hp@mphasis.com>
2026-04-03 17:43:28 +0530

    Add files via upload

---
+++ SpringCloudService2/src/main/java/com/test/controller/Service2Controller.java
@@ -0,0 +1,15 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service2")
+public class Service2Controller {
+	
+	@GetMapping("/upi")
+	public String upiPayments() {
+		return "Spring Cloud Service2 UPI payments";
+	}
+}
