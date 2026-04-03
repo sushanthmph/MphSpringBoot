982fe67b7ddcee1f65bdb020e05b32b2cd544967
sushanthmph <sushanth.hp@mphasis.com>
2026-04-03 17:43:01 +0530

    Add files via upload

---
+++ SpringCloudService1/src/main/java/com/test/controller/Service1Controller.java
@@ -0,0 +1,14 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service1")
+public class Service1Controller {
+	@GetMapping("/openac")
+	public String accCreateService() {
+		return "Service1 Controller AC create operations";
+	}
+}
