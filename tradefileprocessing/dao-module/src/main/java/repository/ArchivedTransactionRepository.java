d309a846d68feea053a13575e43ea948ca6464f9
sushanthmph <sushanth.hp@mphasis.com>
2026-05-09 12:01:43 +0530

    Delete tradefileprocessing directory

--- tradefileprocessing/dao-module/src/main/java/repository/ArchivedTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.ArchivedTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArchivedTransactionRepository extends JpaRepository<ArchivedTransaction,Long> {
-}
--- tradefileprocessing/dao-module/src/main/java/repository/TransactionMetadataRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.TransactionMetadata;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionMetadataRepository extends JpaRepository<TransactionMetadata,Long>{
-}
